517b677029f8a4a9db7a7184fa586d3fe3909dc8
DimitriGoderidze94 <dimitrigoderidze.com>
2024-09-28 11:45:13 +0400

    br-1 testing tailwind grid

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -6,19 +6,19 @@ const About = () => {
         <div className={style.about}>
             <div className="grid grid-cols-4 gap-4">
                 <div className="text-3xl font-bold underline">01</div>
-                <div>09</div>
                 <div>01</div>
-                <div>09</div>
-                <div>01</div>
-                <div>09</div>
-                <div>01</div>
-                <div>09</div>
-                <div>01</div>
-                <div>09</div>
-                <div>01</div>
-                <div>09</div>
-                <div>01</div>
-                <div>09</div>
+                <div>02</div>
+                <div>03</div>
+                <div>04</div>
+                <div>05</div>
+                <div>06</div>
+                <div>07</div>
+                <div>08</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div>14</div>
             </div>
         </div>
     );
