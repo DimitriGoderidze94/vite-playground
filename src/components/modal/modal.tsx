d73786f35c266515e79b661aaed3279855f1dd03
DimitriGoderidze94 <dimitrigoderidze.com>
2024-10-04 15:09:16 +0400

    br-3 added animation on modal opening

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -6,11 +6,14 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children }) => {
-    if (!isOpen) return null;
-
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-            <div className="bg-white rounded-lg shadow-lg w-1/3 p-6">
+        <div
+            className={`fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50 transition-opacity duration-300 ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}
+        >
+            <div
+                className={`bg-white rounded-lg shadow-lg w-1/3 p-6 transform transition-transform duration-300 ${isOpen ? 'scale-100' : 'scale-75'
+                    }`}
+            >
                 <div className="flex justify-between items-center">
                     <h2 className="text-xl font-semibold">{title}</h2>
                     <button
