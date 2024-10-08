469ae4c925fe3d9683ee2d7680c3df255f6f2286
DimitriGoderidze94 <dimitrigoderidze.com>
2024-10-08 17:14:59 +0400

    br-4 minor style

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -40,8 +40,8 @@ const List: React.FC = () => {
                                         ref={provided.innerRef}
                                         {...provided.draggableProps}
                                         {...provided.dragHandleProps}
-                                        className={`border-2 border-blue-500 p-3 rounded-md transition-all duration-300 ease-in-out transform ${snapshot.isDragging ? 'bg-blue-500 text-white shadow-lg scale-105' : 'bg-blue-100'
-                                            }`}
+                                        className={`border-2 border-blue-500 p-3 rounded-md transition-all duration-300 ease-in-out transform 
+                                            ${snapshot.isDragging ? 'bg-blue-500 text-white shadow-lg scale-105' : 'bg-blue-100 hover:bg-blue-500 hover:text-white hover:shadow-lg hover:scale-105'}`}
                                     >
                                         {item}
                                     </div>
