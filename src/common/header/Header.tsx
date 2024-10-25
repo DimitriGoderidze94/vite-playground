658c31cdc7094a52df57a8f806526e37eb15d730
DimitriGoderidze94 <dimitrigoderidze.com>
2024-10-25 10:33:53 +0400

    br-6 responsive fixes

--- src/common/header/Header.tsx
+++ src/common/header/Header.tsx
@@ -5,9 +5,9 @@ const Header = () => {
     return (
         <header className={`flex shadow-md py-4 px-4 sm:px-10 bg-blue-100 font-[sans-serif] min-h-[70px] tracking-wide relative z-50 mb-5 ${style.header}`}>
             <div className='flex flex-wrap items-center justify-between gap-5 w-full'>
-                <a href=""><img src="https://readymadeui.com/readymadeui.svg" alt="logo" className='w-36' />
+                <a href="">
+                    <img src="https://readymadeui.com/readymadeui.svg" alt="logo" className='sm:block  w-36 hidden' />
                 </a>
-
                 <div id="collapseMenu"
                     className='max-lg:hidden lg:!block max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50'>
                     <button id="toggleClose" className='lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white p-3'>
@@ -42,9 +42,9 @@ const Header = () => {
                         className='px-4 py-2 text-sm rounded-full font-bold text-white border-2 border-[#007bff] bg-[#007bff] transition-all ease-in-out duration-300 hover:bg-transparent hover:text-[#007bff]'>Sign
                         up</button>
 
-                    <button id="toggleOpen" className='lg:hidden'>
+                    <button id="toggleOpen" className='lg:hidden border-blue-300'>
                         <svg className="w-7 h-7" fill="#000" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path fillRule="evenodd"
+                            <path fillRule="evenodd" fill="blue"
                                 d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
                                 clipRule="evenodd"></path>
                         </svg>
