3e8e46c643100e9a7c553b7b18ee86710f7280f3
DimitriGoderidze94 <dimitrigoderidze.com>
2024-10-29 17:30:37 +0400

    br-7 changed background

--- src/common/header/Header.tsx
+++ src/common/header/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
     return (
-        <header className={`flex shadow-md py-4 px-4 sm:px-10 bg-blue-200 font-[sans-serif] min-h-[70px] tracking-wide relative z-50 mb-5 ${style.header} ${isOpen ? style.hidden : ''}`}>
+        <header className={`flex shadow-md py-4 px-4 sm:px-10 font-[sans-serif] min-h-[70px] tracking-wide relative z-50 mb-5 ${style.header} ${isOpen ? style.hidden : ''}`}>
             <div className='flex flex-wrap items-center justify-between gap-5 w-full'>
                 <a href="">
                     <img src="https://readymadeui.com/readymadeui.svg" alt="logo" className='sm:block  w-36 hidden' />
