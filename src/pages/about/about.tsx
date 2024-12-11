bf3f7eff431d14ddbbf1a767dddf909f6330bdb2
DimaGode <dimitrigoderidze@gmail.com>
2024-12-11 17:55:12 +0400

    br-8 adding graphql and apollo

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,42 +1,59 @@
 import { useState } from 'react';
-import toast, { Toaster } from 'react-hot-toast';
+// import toast, { Toaster } from 'react-hot-toast';
 import style from './about.module.scss';
-import { Button, Modal } from '../../components';
-import { useDispatch, useSelector } from 'react-redux';
-import { bindActionCreators } from '@reduxjs/toolkit';
-import { actionCreators, State } from '../../state';
+// import { Button, Modal } from '../../components';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { bindActionCreators } from '@reduxjs/toolkit';
+// import { actionCreators, State } from '../../state';
+import { useQuery, gql } from '@apollo/client';
+
+
+const GET_CHARACTERS = gql`
+query {
+  characters {
+    results {
+      id
+      name
+      image
+    }
+  }
+}
+`;
+
 
 const About = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    // const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const amount = useSelector((state: State) => state.bank)
+    // const amount = useSelector((state: State) => state.bank)
 
-    const openModal = () => {
-        setIsModalOpen(true);
-        toast.success('Data loaded successfully!', {
-            className: 'font-bold rounded-lg p-4 shadow-lg',
-            duration: 4000,
-            iconTheme: {
-                primary: '#4ade80',
-                secondary: '#fff',
-            },
-        });
-    };
+    // const openModal = () => {
+    //     setIsModalOpen(true);
+    //     toast.success('Data loaded successfully!', {
+    //         className: 'font-bold rounded-lg p-4 shadow-lg',
+    //         duration: 4000,
+    //         iconTheme: {
+    //             primary: '#4ade80',
+    //             secondary: '#fff',
+    //         },
+    //     });
+    // };
 
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
+    // const closeModal = () => {
+    //     setIsModalOpen(false);
+    // };
 
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
 
-    const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(actionCreators, dispatch);
+    // const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(actionCreators, dispatch);
 
-    const [chooseDepositAmount, setDepositChooseAmount] = useState<number | ''>(1);
-    const [chooseWithdrawAmount, setWithdrawChooseAmount] = useState<number | ''>(1);
+    // const [chooseDepositAmount, setDepositChooseAmount] = useState<number | ''>(1);
+    // const [chooseWithdrawAmount, setWithdrawChooseAmount] = useState<number | ''>(1);
+
+    const something = useQuery(GET_CHARACTERS);
 
     return (
         <div className={style.about}>
-            <div className={style['amount-holder']}>
+            {/* <div className={style['amount-holder']}>
                 <span className={style.bank}>Bank</span>
                 <span className={style.amount}>{amount}</span>
             </div>
@@ -65,7 +82,7 @@ const About = () => {
             </div>
             <Modal isOpen={isModalOpen} onClose={closeModal} title="My Modal">
                 <p>current amount: {amount}</p>
-            </Modal>
+            </Modal> */}
         </div>
     );
 };
